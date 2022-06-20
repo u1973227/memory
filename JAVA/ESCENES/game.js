d40f015d9cf181b2740aa2ee01ef2fc42d55a18b
u1973227 <100150954+u1973227@users.noreply.github.com>
2022-06-20 15:11:06 +0200

    Update game.js

--- JAVA/ESCENES/game.js
+++ JAVA/ESCENES/game.js
@@ -60,7 +60,7 @@ class GameScene extends Phaser.Scene {
 		var m = 0;
 		for (let n = 0; n < columnes; n++){
 			for(let k = 0; k < files; k++){
-				this.add.image(n * 125 + this.cameras.main.centerX - (cartes / 2)*100, k * 150 + this.cameras.main.centerY - (cartes * 128)/4, arraycards[m]);
+				this.add.image(n * 125 + this.cameras.main.centerX - (cartes / 2)*100, k * 150 + this.cameras.main.centerY - (cartes * 128)/4, arraycards_joc[m]);
 				m += 1;
 			}
 		}
@@ -91,7 +91,7 @@ class GameScene extends Phaser.Scene {
 						let c = 0;
 						for (let i = 0; i < columnes; i++){
 							for(let j = 0; j < files; j++){
-								this.add.image(n * 125 + this.cameras.main.centerX - (cartes / 2)*100, k * 150 + this.cameras.main.centerY - (cartes * 128)/4, arraycards[c]);
+								this.add.image(n * 125 + this.cameras.main.centerX - (cartes / 2)*100, k * 150 + this.cameras.main.centerY - (cartes * 128)/4, arraycards_joc[c]);
 								c += 1;
 								destructor.push(imatge);						
 							}
